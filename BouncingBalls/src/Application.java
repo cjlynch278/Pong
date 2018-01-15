ebe5a0c2ca333521d346795d9e3621e7788e6934
cjlynch278 <cjlynch@smcm.edu>
2018-01-14 21:27:02 -0500

    Update Application.java

--- BouncingBalls/src/Application.java
+++ BouncingBalls/src/Application.java
@@ -1,32 +1,15 @@
-import java.util.Scanner;
+
 import javax.swing.JFrame;
 
 public class Application {	
 	public static void main(String[] args) {
 		@SuppressWarnings("unused")
-		JFrame window,first;
-		boolean error = true;
 		
-		//Retrieve a suitable input
-		while(error == true){
-			try{
-				Scanner scan = new Scanner(System.in);
-				System.out.println("What difficulty would you like(1 - 4 where 4 is the hardest): ");
-				int input = scan.nextInt();
+		//Creates the frame
+		JFrame bouncyBallFrame;
+		bouncyBallFrame = new BouncyBallFrame(700,0,600,600, 0);
+		
 
-				if(input<=0 || input>=5){
-					System.out.println("Please enter a number between 1 - 4");
-				}
-				else{
-					error = false;
-					first  = new BouncyBallFrame(700,0,600,600,input);
-					scan.close();
-			}
-		}
-		catch(Exception e){
-			System.out.println("Error: Please enter a number");	
-		}
-		}	
 	}
 }
 
