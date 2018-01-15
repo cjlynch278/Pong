2b432c2e34d5044d4241fa96c7e4a0eb08444d3e
cjlynch278 <cjlynch@smcm.edu>
2018-01-14 21:28:35 -0500

    Update BouncyBallFrame.java
    Implemented multiple panels

--- BouncingBalls/src/BouncyBallFrame.java
+++ BouncingBalls/src/BouncyBallFrame.java
@@ -6,28 +6,65 @@ import javax.swing.JTextField;
 
 
 @SuppressWarnings("serial")
+
+/*
+ * BouncyBallFrame creates the frame for the game. This class will hold all of the panels, and 
+ * 		organizes them all. It will determine which panel to display.
+ */
 public class BouncyBallFrame extends JFrame implements KeyListener {
 
-	BouncyBallsPanel panel;
-	 
+	PongGame panel;
+	int width;
+	int height;
+	int difficulty;
+	EndGamePanel endGamePanel;
+	Instructions instructions;
 	
-	public BouncyBallFrame (int x, int y, int width, int height, int difficulty) {
 	
+	public BouncyBallFrame (int x, int y, int w, int h, int d) {
+
+		//Creates all of the properties of the frame
+		
+		//Set the dimensions of the frame
+		width = w;
+		height = h;
+		difficulty = d;
+		setLocation(x, y);
+		setSize(width, height);
+		
+		//The typing area is needed to move the players paddle
 		JTextField typingArea = new JTextField(20);
 		typingArea.addKeyListener(this);
 		add(typingArea,BorderLayout.SOUTH);
 		
-		setSize(width, height);
-		setLocation(x, y);
+		//Create the home panel and add that to the frame, this will be the 
+		// first panel
+		Home home = new Home(this);
+		setVisible(true);
+		add(home);
+		home.setVisible(true);
+
+		//Make the player unable to change the dimensions of the window
+		setResizable(false);
 		
-		//Set up all of the objects
-		panel = new BouncyBallsPanel(width,height,difficulty);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+	
+	//Calls the necessary methods to start the game of pong.
+	public void startPong(double difficulty) {
+		panel = new PongGame(width,height,difficulty, this);
 		add(panel, BorderLayout.CENTER);
 		setVisible(true);
 		panel.makeObjects();
-		panel.paddleCollision();
-
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	//Creates the end game panel and displays it
+	public void endGame(char result,double difficulty) {
+		endGamePanel = new EndGamePanel(difficulty,result, this);
+		add(endGamePanel, BorderLayout.CENTER);
+		setVisible(true);
+		
 	}
 	@Override
 	public void keyPressed(KeyEvent e) {
